559aa251d81aae81a2cfb2358e279925d8868120
Mathieu Leger-Dalcourt <Mathieu.Leger-Dalcourt@fujitsu.com>
2018-05-30 15:01:47 -0400

    add control widget

--- map.js
+++ map.js
@@ -1,5 +1,15 @@
 mapboxgl.accessToken = ACCESS_TOKEN;
-var map = new mapboxgl.Map({
+const fujitsu = [-71.306222, 46.830545];
+const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center: fujitsu,
+    zoom: 15
 });
+
+map.on('load', function () {
+    // Add zoom/compass 
+    map.addControl(new mapboxgl.NavigationControl());
+
+
+})
